3d0476d8fb26a0030bcc472d137311252a5403ea
Hitarth <hitarthchudgar@gmail.com>
2021-01-16 00:09:39 -0500

    Added the sound playing

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll('.sounds');
+    const pads = document.querySelectorAll('.pads div');
+
+    //play sound on click
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            //rest to start
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
+});
